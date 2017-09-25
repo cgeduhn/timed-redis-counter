e6825baaf8d9ea9661b4309340c61e87edd487cb
Christopher Geduhn <christopher.geduhn@ticketpay.de>
2017-09-25 14:28:21 +0200

    weitere Verbesserungen

--- timed-rediscounter/lib/timed/rediscounter/counter.rb
+++ timed-rediscounter/lib/timed/rediscounter/counter.rb
@@ -2,15 +2,12 @@ module Timed
   module Rediscounter
     class Counter
 
-      Steps   = [1.hour,  1.day,   1.month,  1.year,  2.year].freeze
       Periods = [:minute, :hour,   :day,     :month,  :year].freeze
-      DefaultPeriods = Periods
 
-      attr_reader :steps,:periods,:key
+      attr_reader :periods,:key
       def initialize(key,default_options={})
         @key = key
-        @periods = (default_options.delete(:periods) || DefaultPeriods)
-        @steps = (default_options.delete(:steps) || Steps).collect(&:to_i).sort
+        @periods = (default_options.delete(:periods) || Periods)
         raise_if_not_valid_periods(@periods)
         @default_options = default_options.to_h
       end
@@ -39,23 +36,28 @@ module Timed
       # Returns a Hash by a given range or a period
       #
       # example:
-      # hash_for_range(1.hour.ago..Time.now)
+      # history(1.hour.ago..Time.now) or history(1.hour.ago)
+      #
       #
       # result:
       # {2017-09-22 15:00:00 +0200=>0, 2017-09-22 16:00:00 +0200=>0} 
       # 
       # optional Parameter period: 
       # [:minute, :hour,   :day,     :month,  :year]
-      def timehash(range,period=nil)
+      def history(range_arg,period=nil)
+        redis_key, hash_keys =  build_redishash_arguments(range_arg,period)
+        return Hash.new if hash_keys.empty?
 
-        period ||= period_by_range(range)
-        ts_array = hash_keys_by_range(period,range)
-        return Hash.new if ts_array.empty?
-
-        redis.mapped_hmget(period_key(period), *ts_array).inject({}) do |h,(k,v)| 
+        redis.mapped_hmget(redis_key, *hash_keys).inject({}) do |h,(k,v)| 
           h[Time.at(k)] = v.to_i
           h
         end
+      end
+
+      def sum(range_arg,period=nil)
+        redis_key, hash_keys =  build_redishash_arguments(range_arg,period)
+        return 0 if hash_keys.empty?
+        redis.hmget(redis_key, *hash_keys).inject(0){|sum,i| sum += i.to_i}
       end
 
       #Expiring all period Keys
@@ -82,36 +84,49 @@ module Timed
 
       private
 
-      #build the hash keys for a period and a range
-      def hash_keys_by_range(period,range)
+      #builds the 
+      def build_redishash_arguments(range_arg,period=nil)
+        case range_arg 
+        when Time,Date
+          range = (range_arg..Time.current)
+        when String
+          range = (Time.parse(range_arg)..Time.current)
+        when Range
+          range = range_arg
+        else
+          ArgumentError.new 
+        end
+        period ||= period_by_range(range)
         raise_if_not_valid_periods(period) 
 
         off_set = 1.send(period)
-        ts_array = Set.new
+        hash_keys = Set.new
 
         start_time = range.first.send("beginning_of_#{period}")
         end_time = range.last.send("beginning_of_#{period}")
 
-        ts_array << start_time
+        hash_keys << start_time
         while (end_time - start_time) >= off_set
           start_time += off_set
-          ts_array << start_time
+          hash_keys << start_time
         end
-        return ts_array.collect(&:to_i)
+
+        return period_key(period), hash_keys.collect(&:to_i)
       end
 
+      Steps   = [1.hour,  1.day,   1.month,  1.year,  2.year].freeze
       # Calculate a a valid period by a given range
       def period_by_range(range)
         diff = (range.last - range.first).round
         period = nil
-        @steps.each_with_index do |step,i|
+        Steps.each_with_index do |step,i|
           if diff <= step
             period = @periods[i] 
             break
           end
         end
         #if not found => fallback
-        period ||= ( @periods[@steps.length] || @periods.last ) 
+        period ||= ( @periods[Steps.length] || @periods.last ) 
       end
 
       def raise_if_not_valid_periods(a)
--- timed-rediscounter/spec/timed/rediscounter_spec.rb
+++ timed-rediscounter/spec/timed/rediscounter_spec.rb
@@ -13,22 +13,23 @@ RSpec.describe Timed::Rediscounter do
 
   describe "as a standard_counter" do 
 
+
     it "has standard periods" do 
       expect(standard_counter.periods).to eq(Timed::Rediscounter::Counter::Periods)
     end
 
     it "should count correctly" do
-      
+
       incr_result = standard_counter.incr
       expect(incr_result).not_to be_empty
       expect(incr_result).not_to be nil
        
       hashes_to_check = {
-        minute: standard_counter.timehash(1.minute.ago..Time.current),
-        hour: standard_counter.timehash(1.hour.ago..Time.current),
-        day:  standard_counter.timehash(1.day.ago..Time.current),
-        month: standard_counter.timehash(1.month.ago..Time.current),
-        year: standard_counter.timehash(13.month.ago..Time.current),
+        minute: standard_counter.history(1.minute.ago..Time.current),
+        hour: standard_counter.history(1.hour.ago..Time.current),
+        day:  standard_counter.history(1.day.ago..Time.current),
+        month: standard_counter.history(1.month.ago..Time.current),
+        year: standard_counter.history(13.month.ago..Time.current),
       }
       
       hashes_to_check.each do |k,h| 
@@ -53,7 +54,55 @@ RSpec.describe Timed::Rediscounter do
       end
 
     end
+
+
+    it "should delete keys correctly" do 
+      incr_result = standard_counter.incr
+      expect(incr_result).not_to be_empty
+      expect(incr_result).not_to be nil
+
+      Timed::Rediscounter::Counter::Periods.each do |period|
+        expect(standard_counter.sum(1.send(period).ago)).to eq(1)
+      end
+
+      standard_counter.delete_keys
+
+      Timed::Rediscounter::Counter::Periods.each do |period|
+        expect(standard_counter.sum(1.send(period).ago)).to eq(0)
+      end
+      
+    end
   end
+
+
+  Timed::Rediscounter::Counter::Periods.each do |period|
+    describe "as #{period} counter" do 
+
+      counter = Timed::Rediscounter::Counter.new("#{period}",periods: [period])
+      it "has only #{period} keys" do 
+         expect(counter.periods).to eq([period])
+      end
+
+      it "counts only #{period}" do 
+        expect(counter.incr).to eq([1])
+      end
+
+      it "has only two keys if range goes from 1 #{period} ago" do 
+        expect(counter.incr).to eq([1])
+        r = counter.history(1.send(period).ago)
+        expect(r.keys.length).to eq(2)
+        expect(r.values.inject(0) { |sum, p| sum + p }).to eq(1)
+      end
+
+    end
+  end
+
+
+
+
+
+
+
 
   it "has a version number" do
     expect(Timed::Rediscounter::VERSION).not_to be nil
